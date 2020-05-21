3d1ba86f58c7a2f6056418902bab89e926bbbd11
mpromonet <michel.promonet@free.fr>
2020-05-21 05:48:29 +0200

    upgrade to angular9

--- angular/README.md
+++ angular/README.md
@@ -1,6 +1,6 @@
-# Angular
+# Ng
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.6.
 
 ## Development server
 
--- angular/src/app/format/format.component.ts
+++ angular/src/app/format/format.component.ts
@@ -50,7 +50,19 @@ import { DOCUMENT } from '@angular/common';
 export class FormatComponent implements OnInit {
 
  	formatList = {};
-	selectedFormat : {};
+	selectedFormat : {
+		format: string,
+		description: string,
+		width: number,
+		height: number,
+		fps: number
+	} = {
+		format: "???",
+		description: "",
+		width: 0,
+		height: 0,
+		fps: 0
+	};
 	
 	constructor(@Inject(DOCUMENT) private _document: any, 
 				private _httpClient: HttpClient) {
@@ -61,16 +73,16 @@ export class FormatComponent implements OnInit {
 		    .subscribe(data => {
 				this.formatList = data;
 				this._httpClient.get(this._document.location.href + "/api/format").subscribe(data => {
-					     this.selectedFormat = data;
+					     (this.selectedFormat as any) = data;
 				});
 		    });
 		    
 	}
 
-	update(format: string, widthstr: string, heightstr: string, fpsstr: string) {
+	update(format: string, widthstr: any, heightstr: any, fpsstr: any) {
 		const width = parseInt(widthstr)
 		const height = parseInt(heightstr)
-		const fps = parseInt(fpsstr)
+		const fps = parseInt(fpsstr)		
 		console.log(format, width, height, fps)
 		for (const prop in this.selectedFormat) {
 			this.selectedFormat[prop] = "???"
