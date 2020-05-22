29d2435432046ee70767f6452a7f82c0b025180f
mpromonet <michel.promonet@free.fr>
2020-05-22 17:22:01 +0100

    allow to give set capture format

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,20 @@ void sighandler(int)
        stop =1;
 }
 
+// -----------------------------------------
+//    convert string video format to fourcc 
+// -----------------------------------------
+int decodeVideoFormat(const char* fmt)
+{
+	char fourcc[4];
+	memset(&fourcc, 0, sizeof(fourcc));
+	if (fmt != NULL)
+	{
+		strncpy(fourcc, fmt, 4);	
+	}
+	return v4l2_fourcc(fourcc[0], fourcc[1], fourcc[2], fourcc[3]);
+}
+
 /* ---------------------------------------------------------------------------
 **  main
 ** -------------------------------------------------------------------------*/
@@ -41,31 +55,34 @@ int main(int argc, char* argv[])
 {	
 	int verbose=0;
 	const char *dev_name = "/dev/video0";	
-	int width = 320;
-	int height = 240;	
+	int width = 640;
+	int height = 480;	
 	int fps = 10;	
 	int c = 0;
 	const char * port = "8080";
 	V4l2Access::IoType ioTypeIn = V4l2Access::IOTYPE_MMAP;
 	std::string webroot = "webroot";
 	std::string nbthreads;
+	unsigned int format = ~0;
+	std::list<unsigned int> videoformatList;	
 	
-	while ((c = getopt (argc, argv, "hv::" "W:H:F:G:" "r" "P:p:N:")) != -1)
+	while ((c = getopt (argc, argv, "hv::" "f:W:H:F:G:" "r" "P:p:N:")) != -1)
 	{
 		switch (c)
 		{
-			case 'v':	verbose = 1; if (optarg && *optarg=='v') verbose++;  break;
+			case 'v': verbose = 1; if (optarg && *optarg=='v') verbose++;  break;
 
-			case 'W':	width = atoi(optarg); break;
-			case 'H':	height = atoi(optarg); break;
-			case 'F':	fps = atoi(optarg); break;
-			case 'G':   sscanf(optarg,"%dx%dx%d", &width, &height, &fps); break;
+			case 'f':	format    = decodeVideoFormat(optarg); if (format) {videoformatList.push_back(format);};  break;
+			case 'W': width = atoi(optarg); break;
+			case 'H': height = atoi(optarg); break;
+			case 'F': fps = atoi(optarg); break;
+			case 'G': sscanf(optarg,"%dx%dx%d", &width, &height, &fps); break;
 
-			case 'r':	ioTypeIn = V4l2Access::IOTYPE_READWRITE; break;			
+			case 'r': ioTypeIn = V4l2Access::IOTYPE_READWRITE; break;			
 
-			case 'P':	port = optarg; break;
-			case 'N':   nbthreads = optarg; break;
-			case 'p':	webroot = optarg; break;			
+			case 'P': port = optarg; break;
+			case 'N': nbthreads = optarg; break;
+			case 'p': webroot = optarg; break;			
 			case 'h':
 			{
 				std::cout << argv[0] << " [-v[v]] [-P port] [-W width] [-H height] [-F fps] [-G <w>x<h>x<f>] [device]" << std::endl;
@@ -74,6 +91,7 @@ int main(int argc, char* argv[])
 				std::cout << "\t -P port          : server port (default "<< port << ")" << std::endl;
 				std::cout << "\t -p path          : server root path (default "<< webroot << ")" << std::endl;
 
+				std::cout << "\t -f format        : V4L2 capture using format" << std::endl;
 				std::cout << "\t -W width         : V4L2 capture width (default "<< width << ")" << std::endl;
 				std::cout << "\t -H height        : V4L2 capture height (default "<< height << ")" << std::endl;
 				std::cout << "\t -F fps           : V4L2 capture framerate (default "<< fps << ")" << std::endl;
@@ -95,12 +113,14 @@ int main(int argc, char* argv[])
 	initLogger(verbose);
 
 	// init V4L2 capture interface
-	std::list<unsigned int> formatList;
-	formatList.push_back(V4L2_PIX_FMT_MJPEG);
-	formatList.push_back(V4L2_PIX_FMT_JPEG);
-	formatList.push_back(V4L2_PIX_FMT_YUYV);
-	formatList.push_back(0);
-	V4L2DeviceParameters param(dev_name,formatList,width,height,fps,verbose);
+	if ((videoformatList.empty()) && (format!=0)) {
+		videoformatList.push_back(V4L2_PIX_FMT_MJPEG);
+		videoformatList.push_back(V4L2_PIX_FMT_JPEG);
+		videoformatList.push_back(V4L2_PIX_FMT_YUYV);
+		videoformatList.push_back(0);
+	}
+
+	V4L2DeviceParameters param(dev_name,videoformatList,width,height,fps,verbose);
 	V4l2Capture* videoCapture =  V4l2Capture::create(param, ioTypeIn);
 	if (videoCapture == NULL)
 	{	
