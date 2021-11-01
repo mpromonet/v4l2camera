16f25d2164a3c2155fc17f9b8cf1ee09c4087c6c
mpromonet <michel.promonet@free.fr>
2021-11-01 21:11:10 +0100

    update value

--- vuejs/src/components/Controls.vue
+++ vuejs/src/components/Controls.vue
@@ -2,17 +2,19 @@
   <div>
     <tr v-for="c in controls">
       <td>{{ c.name }}</td>
-      <td><input v-model="c.value" type="text" /></td>
+      <td>
+		  <input v-model="c.value" type="text" />
+	  </td>
       <td>[ {{ c.minimum }} ,</td>
       <td>{{ c.maximum }} ]</td>
       <td>
-        <select v-if="c.menu" v-model="c.value">
-          <option v-for="m in c.menu">{{ m.label }}</option>
+        <select v-if="c.menu" v-model="c.value" v-on:change="updateValue(c.id,parseInt(c.value))">
+          <option v-for="m in c.menu" v-bind:value="m.value">{{ m.label }}</option>
         </select>
         <input
           v-if="!c.menu && c.minimum == 0 && c.maximum == 1"
-          v-model="c.value"
-          type="checkbox"
+          v-model="c.value" true-value="1" false-value="0"
+          type="checkbox" v-on:change="updateValue(c.id,parseInt(c.value))"
         />
         <input
           v-if="!c.menu && !(c.minimum == 0 && c.maximum == 1)"
@@ -20,7 +22,7 @@
           :min="c.minimum"
           :max="c.maximum"
           type="range"
-          :step="c.step"
+          :step="c.step" v-on:change="updateValue(c.id,parseInt(c.value))"
         />
       </td>
     </tr>
@@ -34,7 +36,7 @@ var serviceurl = "";
 
 export default {
   mounted() {
-    axios({ method: "GET", url: serviceurl + "/api/controls" }).then(
+    axios.get(serviceurl + "/api/controls").then(
       (response) => {
         this.controls = response.data;
       }
@@ -45,6 +47,15 @@ export default {
       controls: [],
     };
   },
+  methods: {
+    updateValue: function(id, value) {
+		axios.post(serviceurl + "/api/control", {id, value} ).then(
+			(response) => {
+				this.controls.filter((d) => d.id == id).value = response.data.value;
+			}
+		);
+	}
+  }
 };
 </script>
 
