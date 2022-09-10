4deb1091f6f2b2bbd86f3c39dff4e0bc69a65548
mpromonet <michel.promonet@free.fr>
2022-09-10 19:49:43 +0200

    add vuetify

--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify();
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
--- vuejs/vite.config.js
+++ vuejs/vite.config.js
@@ -5,12 +5,18 @@ import vue from '@vitejs/plugin-vue'
 import path from "path";
 
 
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from 'vite-plugin-vuetify'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, '../webroot'),
   },
-  plugins: [vue()],
+  plugins: [
+		vue(),
+		vuetify({ autoImport: true }),
+	],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
