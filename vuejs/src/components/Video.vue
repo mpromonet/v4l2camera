13b4812b253a879cdf6ec479fdca9528c71796b4
Michel Promonet <michel.promonet@free.fr>
2024-02-06 21:11:46 +0100

    simplify

--- vuejs/src/components/Video.vue
+++ vuejs/src/components/Video.vue
@@ -31,7 +31,8 @@ export default {
       message: null,
       rtspinfo: null,
       videoCanvas: null,
-      format: null
+      format: null,
+      frameResolved: null
     };
   },
   mounted() {
@@ -54,6 +55,11 @@ export default {
     this.connectWebSocket(wsurl.href);
   },
   destroyed() {
+    console.log("Closing decoder");
+    if (this.ws.decoder) {
+      this.ws.decoder.close();
+    }
+    this.ws.decoder = null;
     console.log("Closing WebSocket");
     if (this.ws) {
       this.ws.close();
@@ -68,96 +74,99 @@ export default {
       axios.get("/api/stop").then( () => this.visibility = false);
     },
     async onMessage(message) {
-        if (typeof message.data === 'string') {
-          this.format = JSON.parse(message.data);
+      const { data } = message;
+      if (typeof data === 'string') {
+        this.format = JSON.parse(data);
 
-        } else if (message.data instanceof ArrayBuffer) {
-          const bytes = new Uint8Array(message.data);
-          if ( (bytes.length > 1) && (bytes[0] === 255) && (bytes[1] === 216)) {
-              // JPEG
-              let binaryStr = "";
-              for (let i = 0; i < bytes.length; i++) {
-                binaryStr += String.fromCharCode(bytes[i]);
-              }
-              this.message = null;
-              const img = new Image();
-              img.src = "data:image/jpeg;base64," + btoa(binaryStr);
-              await new Promise(r => img.onload=r);
-              this.videoCanvas.canvas.width = img.width;
-              this.videoCanvas.canvas.height = img.height;
-              this.videoCanvas.drawImage(img, 0, 0);
-              if (this.ws.decoder) {
-                this.ws.decoder.close();
-              }
-              this.ws.decoder = null;
-          } else if ( (bytes.length > 3) && (bytes[0] === 0) && (bytes[1] === 0) && (bytes[2] === 0) && (bytes[3] === 1)) {
-              // H264
-              if (!this.ws.decoder) {
-                this.ws.decoder = new VideoDecoder({
-                  output: (frame) => {
-                    this.videoCanvas.canvas.width = frame.displayWidth;
-                    this.videoCanvas.canvas.height = frame.displayHeight;
-                    this.videoCanvas.drawImage(frame, 0, 0);
-                    frame.close();
-                  },
-                  error: (e) => console.log(e.message),
-                });
-              }
-
-              const naluType = bytes[4] & 0x1F;
-              if (this.ws.decoder.state !== "configured" && naluType === 7) {
-                  let codec = 'avc1.';
-                  for (let i = 0; i < 3; i++) {
-                      codec += ('00' + bytes[5+i].toString(16)).slice(-2);
-                  }
-                  const config = {codec};
-                  const support = await VideoDecoder.isConfigSupported(config);
-                  if (support.supported) {
-                    this.ws.decoder.configure(config);
-                  } else {
-                    this.message = `${codec} is not supported`;                    
-                  }
-              } 
-              if (this.ws.decoder.state === "configured") {
-                  this.message = null;
-                  const chunk = new EncodedVideoChunk({
-                      timestamp: new Date().getTime(),
-                      type: (naluType === 7) || (naluType === 5) ? "key" : "delta",
-                      data: bytes,
-                  });
-                  this.ws.decoder.decode(chunk);
-              }
-          } else if (this.format) {
-              try {
-                const frame = new VideoFrame(bytes, {
-                    format: this.format.format,
-                    timestamp: performance.now(),
-                    codedWidth: this.format.width,
-                    codedHeight: this.format.height,
-                });            
-                this.videoCanvas.canvas.width = frame.displayWidth;
-                this.videoCanvas.canvas.height = frame.displayHeight;
-                this.videoCanvas.drawImage(frame, 0, 0);
-                frame.close();            
-                this.message = null;
-              } catch (e) {
-                this.message = `${this.format.format} is not supported`;  
-              }
-
-          } else {
-            this.message = 'format not supported';  
-          }
-        }      
+      } else if (data instanceof ArrayBuffer) {
+        const bytes = new Uint8Array(data);
+        try {
+          const frame = await this.onFrame(bytes);
+          this.displayFrame(frame);
+          this.message = null;
+        } catch (e) {
+          this.message = `${this.format.format} is not supported`;  
+        }
+      }      
     },
     connectWebSocket(wsurl) {
         console.log(`Connecting WebSocket to ${wsurl}`);
         this.ws = new WebSocket(wsurl);
         this.ws.binaryType = 'arraybuffer';
         this.ws.onmessage = this.onMessage;
-        this.ws.onclose = () => {
-          console.log(`WebSocket closed, reconnecting to ${wsurl}...`);
-          setTimeout(() => this.connectWebSocket(wsurl), 1000);
-        };
+        this.ws.onclose = () => setTimeout(() => this.connectWebSocket(wsurl), 1000);
+    },
+    displayFrame(frame) {
+        this.videoCanvas.canvas.width = frame.displayWidth;
+        this.videoCanvas.canvas.height = frame.displayHeight;
+        this.videoCanvas.drawImage(frame, 0, 0);
+        frame.close();       
+    },
+    async onH264Frame(bytes) {
+      if (!this.ws.decoder) {
+        this.ws.decoder = new VideoDecoder({
+          output: (frame) => this.frameResolved(frame),
+          error: (e) => console.log(e.message),
+        });
+      }
+
+      const naluType = bytes[4] & 0x1F;
+      if (this.ws.decoder.state !== "configured" && naluType === 7) {
+          let codec = 'avc1.';
+          for (let i = 0; i < 3; i++) {
+              codec += ('00' + bytes[5+i].toString(16)).slice(-2);
+          }
+          const config = {codec};
+          const support = await VideoDecoder.isConfigSupported(config);
+          if (support.supported) {
+            this.ws.decoder.configure(config);
+          } else {
+            this.message = `${codec} is not supported`;                    
+          }
+      } 
+      if (this.ws.decoder.state === "configured") {
+          const chunk = new EncodedVideoChunk({
+              timestamp: performance.now(),
+              type: (naluType === 7) || (naluType === 5) ? "key" : "delta",
+              data: bytes,
+          });
+          this.ws.decoder.decode(chunk);
+          return await new Promise(r => this.frameResolved = r);
+      } else {
+        return Promise.reject();
+      }
+    },
+    async onJPEGFrame(bytes) {
+      let binaryStr = "";
+      for (let i = 0; i < bytes.length; i++) {
+        binaryStr += String.fromCharCode(bytes[i]);
+      }
+      const img = new Image();
+      img.src = "data:image/jpeg;base64," + btoa(binaryStr);
+      await new Promise(r => img.onload=r);
+      return new VideoFrame(img, {timestamp: performance.now()});
+    },
+    onDefaultFrame(bytes) {
+      return new VideoFrame(bytes, {
+                  format: this.format.format,
+                  timestamp: performance.now(),
+                  codedWidth: this.format.width,
+                  codedHeight: this.format.height,
+        });    
+    },
+    onFrame(bytes) {
+      if ( (bytes.length > 1) && (bytes[0] === 255) && (bytes[1] === 216)) {
+        return this.onJPEGFrame(bytes);
+
+      } else if ( (bytes.length > 3) && (bytes[0] === 0) && (bytes[1] === 0) && (bytes[2] === 0) && (bytes[3] === 1)) {
+        return this.onH264Frame(bytes);
+
+      } else if (this.format) {
+        return this.onDefaultFrame(bytes);
+
+      } else {
+        return Promise.reject();  
+      }
     }
   }
 };
