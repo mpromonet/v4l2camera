84c036a525055a3fc72d45f09ede30ee9ef660e6
Michel Promonet <michel.promonet@free.fr>
2024-09-20 16:28:08 +0200

    add debouncing for rtspuri

--- vuejs/src/components/Rtsp.vue
+++ vuejs/src/components/Rtsp.vue
@@ -9,14 +9,14 @@
       <v-row>
         <v-col>RTSP Uri</v-col>
         <v-col cols="8">
-          <v-text-field v-model="rtspinfo.rtspuri" @update:modelValue="updateRtspInfo()">
+          <v-text-field v-model="debouncedRtspUri" @keydown="handleKeydown('rtspuri', $event)" :class="{ 'debouncing': isDebouncingRtspUri }" >
           </v-text-field>
         </v-col>
       </v-row>
       <v-row>
         <v-col>Multicast Uri</v-col>
         <v-col cols="8">
-          <v-text-field v-model="rtspinfo.multicasturi" @update:modelValue="updateRtspInfo()">
+          <v-text-field v-model="debouncedMulticastUri" @keydown="handleKeydown('multicasturi', $event)" >
           </v-text-field>
         </v-col>
       </v-row>
@@ -53,11 +53,13 @@
 
 <script>
 import axios from "axios";
+import { debounce } from 'lodash';
 
 export default {
   data() {
     return {
-      rtspinfo: {rtspuri: '', multicasturi: '', url: '', issrtp: false, issrtpencrypted: false, issrtsps: false}
+      rtspinfo: {rtspuri: '', multicasturi: '', url: '', issrtp: false, issrtpencrypted: false, issrtsps: false},
+      isDebouncingRtspUri: false
     };
   },
   mounted() {
@@ -65,14 +67,48 @@ export default {
       (response) => this.rtspinfo = response.data
     );
   },
+  computed: {
+    debouncedMulticastUri: {
+      get() {
+        return this.rtspinfo.multicasturi;
+      },
+      set: debounce(function (value) {
+        this.rtspinfo.multicasturi = value;
+        this.updateRtspInfo();
+      }, 1000) 
+    },
+    debouncedRtspUri: {
+      get() {
+        return this.rtspinfo.rtspuri;
+      },
+      set(value) {
+        this.isDebouncingRtspUri = true;
+        this.debouncedSetRtspUri(value);
+      }
+    }    
+  },  
   methods: {
     async updateRtspInfo() {
         const response = await axios.post("/api/rtspinfo", this.rtspinfo);
-        this.rtspinfo = response.data;      
-    }
+        this.rtspinfo = response.data;  
+        this.isDebouncingRtspUri = false;    
+    },
+    handleKeydown(field, event) {
+      if (event.key === 'Enter') {
+        this.rtspinfo[field] = event.target.value;
+        this.updateRtspInfo();
+      }
+    },
+    debouncedSetRtspUri: debounce(function (value) {
+      this.rtspinfo.rtspuri = value;
+      this.updateRtspInfo();
+    }, 1000)      
   }
 };
 </script>
 
 <style>
+.debouncing {
+  color: red; 
+}
 </style>
